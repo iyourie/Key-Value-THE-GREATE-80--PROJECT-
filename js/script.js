0377648f79accf1a2e9043a547cab603011de87a
iyourie <azhiedif@gmail.com>
2022-08-08 11:56:23 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+window.addEventListener('keypress', (e) => {
+    document.querySelector('h1').innerHTML = `the key you pressed is : ${e.key}`
+    document.querySelector('h3').innerHTML = `its key code is : ${e.keyCode}`
+})
