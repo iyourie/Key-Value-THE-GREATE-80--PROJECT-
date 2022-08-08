289875b690ffc78e674b4e65ddfdd82fc8e42974
iyourie <azhiedif@gmail.com>
2022-08-08 12:17:19 +0100

    .

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+window.alert('press a key')
+
 window.addEventListener('keypress', (e) => {
     document.querySelector('h1').innerHTML = `the key you pressed is : ${e.key}`
     document.querySelector('h3').innerHTML = `its key code is : ${e.keyCode}`
